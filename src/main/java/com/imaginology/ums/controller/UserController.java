999791879875ed6869f1d3985883228e39e865cd
nikhildahal <nikhildahal9@gmail.com>
2019-04-10 19:18:40 +0545

    entity controller and service added

--- src/main/java/com/imaginology/ums/controller/UserController.java
+++ src/main/java/com/imaginology/ums/controller/UserController.java
@@ -1,5 +1,20 @@
 package com.imaginology.ums.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.imaginology.ums.entity.User;
+
 public class UserController {
+	@PostMapping
+	@ResponseStatus(code=HttpStatus.CREATED)
+	public List<User> createUser(@RequestBody User user){
+		return userService.createUser(user);
+	}
+
 
 }
--- src/main/java/com/imaginology/ums/service/UserService.java
+++ src/main/java/com/imaginology/ums/service/UserService.java
@@ -1,5 +1,20 @@
 package com.imaginology.ums.service;
 
-public class UserService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.imaginology.ums.entity.User;
+import com.imaginology.ums.repository.UserRepository;
+
+
+
+public class UserService {
+	@Autowired
+	UserRepository userRepository;
+	public List<User> createTeacherEntity(User user){
+		userRepository.save(user);
+		List<User> user=userRepository.findAll();
+		return user;
+}
 }
