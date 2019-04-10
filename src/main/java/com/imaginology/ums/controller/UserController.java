5ef41fee845efde101373687cc5bba55b3b80c6d
khadka <raushanKhadka@gmail.com>
2019-04-10 20:05:17 +0545

    get all user and get one user created

--- src/main/java/com/imaginology/ums/controller/UserController.java
+++ src/main/java/com/imaginology/ums/controller/UserController.java
@@ -2,19 +2,30 @@ package com.imaginology.ums.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.imaginology.ums.entity.User;
-
+import com.imaginology.ums.service.UserService;
+@RestController
+@RequestMapping("/users")
 public class UserController {
-	@PostMapping
+	@Autowired
+	UserService userService;
 	@ResponseStatus(code=HttpStatus.CREATED)
 	public List<User> createUser(@RequestBody User user){
 		return userService.createUser(user);
 	}
-
-
+@RequestMapping(value="getalluser",method=RequestMethod.Get)
+public List<User>getAllUsers(){
+	return userService.getAllUsers();
+}
+@RequestMapping(value="{userId}",method=RequestMethod.Get){
+	return userService.getUser(id);
+}
 }
--- src/main/java/com/imaginology/ums/service/UserService.java
+++ src/main/java/com/imaginology/ums/service/UserService.java
@@ -3,7 +3,6 @@ package com.imaginology.ums.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-<<<<<<< HEAD
 import org.springframework.stereotype.Service;
 
 import com.imaginology.ums.entity.User;
@@ -16,21 +15,11 @@ UserRepository userRepository;
  List<User> getAllUsers()
 {
 	return userRepository.findAll();
-=======
-
-import com.imaginology.ums.entity.User;
-import com.imaginology.ums.repository.UserRepository;
-
-
-
-public class UserService {
-	@Autowired
-	UserRepository userRepository;
-	public List<User> createUser(User user){
-		userRepository.save(user);
-		List<User> user=userRepository.findAll();
-		return user;
 }
->>>>>>> 793c4bcf8c4dcb792b94d36f28759ea57ac935a4
+
+
+public getUser(Long id) {
+	return userRepository.findById(id)
 }
+	
 }
