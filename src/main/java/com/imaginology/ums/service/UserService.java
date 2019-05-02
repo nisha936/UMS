ffbd89de7a3e9073a6a2be0b7f255d7235291e8f
nikhildahal <nikhildahal9@gmail.com>
2019-05-02 17:29:50 +0545

    error free all

--- src/main/java/com/imaginology/ums/service/UserService.java
+++ src/main/java/com/imaginology/ums/service/UserService.java
@@ -28,14 +28,7 @@ public class UserService {
 	public void deleteUser(Long id) {
 		userRepository.deleteById(id);
 	}
-}
 
-
-
-@Service
-public class UserService {
-@Autowired
-UserRepository userRepository;
  public List<User> getAllUsers() {
 		return userRepository.findAll();
 	}
@@ -44,12 +37,4 @@ public Optional<User> getOneUser(Long id) {
 	return userRepository.findById(id);
 }
 }
-	
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-}
-=======
->>>>>>> 6071f2976114e1b6221c068f69ef47771abe1466
->>>>>>> 577b451958a175ed4745eed5a57d34274c8d5594
->>>>>>> 18b11be6b02e9cbd5011626d5310b8974997c0f8
+
