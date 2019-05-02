fe5e9cd320bb349edd40347dc608be71cdb2f244
nikhildahal <nikhildahal9@gmail.com>
2019-05-02 16:51:35 +0545

    commited

--- src/main/java/com/imaginology/ums/service/UserService.java
+++ src/main/java/com/imaginology/ums/service/UserService.java
@@ -7,36 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.imaginology.ums.entity.User;
 import com.imaginology.ums.repository.UserRepository;
-<<<<<<< HEAD
 import com.imaginology.ums.entity.User;
 import com.imaginology.ums.repository.UserRepository;
 
-@Service
 
-public class UserService {
-	@Autowired
-	UserRepository userRepository;
-	public List<User> createUser(User user){
-		userRepository.save(user);
-		List<User> users=userRepository.findAll();
-		return users;
-}
-}
-=======
-
-@Service
-public class UserService {
-@Autowired
-UserRepository userRepository;
- List<User> getAllUsers()
-{
-	return userRepository.findAll();
-}
-
-
-public getUser(Long id) {
-	return userRepository.findById(id)
-}
-	
-}
->>>>>>> 5ef41fee845efde101373687cc5bba55b3b80c6d
